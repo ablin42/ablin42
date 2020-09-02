d49c98a546ab4b5d96973fc15cf54a9e6f24c8df
ablin <space6fic@gmail.com>
2020-09-02 17:34:41 +0200

    set up fullpage.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 //const cors = require("cors");
-//const helmet = require("helmet");
+const helmet = require("helmet");
 const session = require("express-session");
 const flash = require("express-flash");
 const sanitize = require("mongo-sanitize");
@@ -18,66 +18,70 @@ app.set("trust proxy", 1);
 
 //-- Express Session --//
 app.use(
-  session({
-    /*store: new MongoStore({
+	session({
+		/*store: new MongoStore({
 			mongooseConnection: mongoose.connection,
 			ttl: 365 * 24 * 60 * 60
 		}),*/
-    name: "ablin42",
-    secret: process.env.SESSION_SECRET,
-    resave: true,
-    //proxy: true,
-    saveUninitialized: true,
-    cookie: {
-      path: "/",
-      maxAge: 14 * 24 * 60 * 60 * 1000,
-      httpOnly: false,
-      secure: false,
-    }, //secure = true (or auto) requires https else it wont work
-    //sameSite: "Lax",
-  })
+		name: "ablin42",
+		secret: process.env.SESSION_SECRET,
+		resave: true,
+		//proxy: true,
+		saveUninitialized: true,
+		cookie: {
+			path: "/",
+			maxAge: 14 * 24 * 60 * 60 * 1000,
+			httpOnly: false,
+			secure: false
+		} //secure = true (or auto) requires https else it wont work
+		//sameSite: "Lax",
+	})
 );
 
-/*Helmet
+//Helmet;
 app.use(helmet());
 app.use(helmet.permittedCrossDomainPolicies());
-//app.use(helmet.referrerPolicy({ policy: "same-origin" }));
+app.use(helmet.referrerPolicy({ policy: "same-origin" }));
 app.use(
-  helmet.contentSecurityPolicy({
-    directives: {
-      reportUri: "/report-violation",
-      defaultSrc: ["'self'"],
-      styleSrc: [
-        "'self'",
-        "stackpath.bootstrapcdn.com",
-        "kit-free.fontawesome.com",
-        "fonts.googleapis.com",
-      ],
-      fontSrc: [
-        "'self'",
-        "fonts.googleapis.com",
-        "kit-free.fontawesome.com",
-        "fonts.gstatic.com",
-      ],
-      scriptSrc: [
-        "'self'",
-        "kit.fontawesome.com",
-        "stackpath.bootstrapcdn.com",
-        "www.gstatic.com",
-        "cdn.jsdelivr.net",
-      ],
-      frameSrc: [],
-      imgSrc: ["'self'"],
-    },
-    reportOnly: true,
-  })
-);*/
+	helmet.contentSecurityPolicy({
+		directives: {
+			reportUri: "/report-violation",
+			defaultSrc: ["'self'"],
+			styleSrc: ["'self'", "stackpath.bootstrapcdn.com", "kit-free.fontawesome.com", "fonts.googleapis.com"],
+			fontSrc: ["'self'", "fonts.googleapis.com", "kit-free.fontawesome.com", "fonts.gstatic.com"],
+			scriptSrc: [
+				"'self'",
+				"kit.fontawesome.com",
+				"stackpath.bootstrapcdn.com",
+				"www.gstatic.com",
+				"cdn.jsdelivr.net",
+				"cdnjs.cloudflare.com",
+				"stackpath.bootstrapcdn.com",
+				"kit.fontawesome.com",
+				"'sha256-/cUBdVL3Z8rqgK/i62eIdBoFNnPghvrSMIUOLIgqn8s='"
+			],
+			frameSrc: [],
+			imgSrc: ["'self'"]
+		},
+		reportOnly: false
+	})
+);
 
 //app.use(cors());
 app.use(flash());
 
+app.post("/report-violation", (req, res) => {
+	if (req.body) {
+		console.log("CSP Violation: ", req.ip, req.body);
+	} else {
+		console.log("CSP Violation: No data received!", req.ip);
+	}
+
+	res.status(204).end();
+});
+
 app.get("/", (req, res) => {
-  return res.status(200).render("index");
+	return res.status(200).render("index");
 });
 
 const port = process.env.PORT;
