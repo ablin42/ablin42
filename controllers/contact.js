4d2a00a0723e9e64f6ac3b6b13c6b1208e33c7f3
ablin <space6fic@gmail.com>
2022-05-29 23:09:07 +0200

    Added a blacklist for words

--- controllers/contact.js
+++ controllers/contact.js
@@ -7,6 +7,7 @@ const MongoStore = require("rate-limit-mongo");
 const mailer = require("./helpers/mailer");
 require("dotenv").config();
 const utils = require("./helpers/utils");
+const blacklist = require("../blacklist.json")
 
 const limiter = rateLimit({
 	store: new MongoStore({
@@ -28,7 +29,7 @@ router.post("/", limiter, vContact, async (req, res) => {
 		const content = req.body.content;
 		await utils.checkValidity(req);
 		
-		if (content.includes("battletech")) throw new Error("Spam detected");
+		if (blacklist.some(item => content.includes(item))) throw new Error("Spam detected");
 		if (await mailer(process.env.EMAIL, subject, content)) throw new Error("An error occured while sending the mail");
 
 		return res.status(200).json({ error: false, message: "Your mail has been sent, I will reply as soon as possible!" });
