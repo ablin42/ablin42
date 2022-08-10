d69aad7771254cf10cc86594ab130d0093266027
ablin <space6fic@gmail.com>
2022-08-10 04:20:20 +0200

    Updated config & css url paths

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
 import "@fortawesome/fontawesome-svg-core/styles.css";
-// import { config } from "@fortawesome/fontawesome-svg-core";
-// config.autoAddCss = false; // Tell Font Awesome to skip adding the CSS automatically since it's being imported above
+import { config } from "@fortawesome/fontawesome-svg-core";
+config.autoAddCss = false; // Tell Font Awesome to skip adding the CSS automatically since it's being imported above
 import "bootstrap/dist/css/bootstrap.min.css";
 import "../public/blobz.min.css";
 
